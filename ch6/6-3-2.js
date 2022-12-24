c65145f74e19e3a406a3dd6e7f9de4f56e21cab8
zerom-H <cupdisin@gmail.com>
2022-12-24 23:48:50 +0900

    refactor: 변수 추출하기 - 클래스 전체에 영향을 줄 때 메서드로

--- ch6/6-3-2.js
+++ ch6/6-3-2.js
@@ -11,10 +11,18 @@ export class Order {
   }
 
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    );
+    return  this.basePrice() - this.quantityDiscount() + this.shipping();
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
   }
 }
