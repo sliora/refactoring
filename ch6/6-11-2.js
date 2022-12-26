66ca726933832c1d621cb5dae27372d2ff219cd7
zerom-H <cupdisin@gmail.com>
2022-12-26 20:22:07 +0900

    refactor: 단계 쪼개기

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,22 @@
 import fs from 'fs';
 
-if (!process.argv[2]) {
+const fileName = process.argv[2];
+if (!fileName) {
   throw new Error('파일 이름을 입력하세요');
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
+const filePath = `./${fileName}.json`;
+if (!fs.existsSync(filePath)) {
   throw new Error('파일이 존재하지 않습니다');
 }
 
-const rawData = fs.readFileSync(fileName);
+const rawData = fs.readFileSync(filePath);
 const orders = JSON.parse(rawData);
+
+let count;
 if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
+  count = orders.filter((order) => order.status === 'ready').length;
 } else {
-  console.log(orders.length);
+  count = orders.length;
 }
+console.log(count);
