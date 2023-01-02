a0818d18a72a9c7d4751796006eb4d7757fc6722
zerom-h <cupdisin@gmail.com>
2023-01-02 11:08:23 +0900

    refactor: 변수 캡슐화하기, private 변수 _ -> # 변경

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,24 +1,20 @@
-let defaultOwnerData = { firstName: '마틴', lastName: '파울러' };
-
-export function defaultOwner() {
-  return new Person(defaultOwnerData);
-}
-
-export function setDefaultOwner(arg) {
-  defaultOwnerData = arg;
-}
-
 class Person {
+  #lastName;
+  #firstName;
   constructor(data) {
-    this._lastName = data.lastName;
-    this._firstName = data.firstName;
+    this.#lastName = data.lastName;
+    this.#firstName = data.firstName;
   }
-
   get lastName() {
-    return this._lastName
+    return this.#lastName;
   }
-
   get firstName() {
-    return this._firstName
+    return this.#firstName;
   }
+}
+
+let defaultOwner = new Person({ firstName: '마틴', lastName: '파울러' });
+
+export function getDefaultOwner() {
+  return defaultOwner;
 }
