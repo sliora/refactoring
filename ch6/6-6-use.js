729de6bf7090fce63dbf3eff8618900d1519e125
zerom-h <cupdisin@gmail.com>
2023-01-02 11:08:38 +0900

    refactor: 변수 캡슐화하기 사용

--- ch6/6-6-use.js
+++ ch6/6-6-use.js
@@ -1,11 +1,5 @@
-import {defaultOwner, setDefaultOwner} from './6-6.js';
+import { getDefaultOwner } from './6-6.js';
 
-console.log(defaultOwner());
-
-const owner = defaultOwner();
-//owner.firstName = '엘리';
-console.log(owner);
-
-const owner2 = defaultOwner()
-//owner.lastName = "파슨스";
-console.log(owner2)
+const owner = getDefaultOwner();
+console.log(owner.firstName);
+console.log(getDefaultOwner());
