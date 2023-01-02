b21c4ef9d2331102deeec721e8dbed86c3a69f86
zerom-h <cupdisin@gmail.com>
2023-01-02 11:07:49 +0900

    refactor: 매개변수 추가하기, 불필요한 임시변수 삭제

--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,5 +1,3 @@
-const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));
 export function inNewEngland(stateCode) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
-
 }
