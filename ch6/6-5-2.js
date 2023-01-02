01a31c67a1c6afac44013d090069363909dada8e
zerom-h <cupdisin@gmail.com>
2023-01-02 11:07:13 +0900

    refactor: 함수 이름 바꾸기(마이그레이션 절차), 파라미터 변경

--- ch6/6-5-2.js
+++ ch6/6-5-2.js
@@ -4,22 +4,13 @@ export default class Book {
     this.#reservations = [];
   }
 
-  addReservation(customer) {
-    this.#reservations.push(customer);
-  }
-
-  /**
-   * 185p 이상태에서 기존 함수를 인라인하여 호출 코드들이 새 함수를 이용하도록 고침
-   * 이해가 안가서 강의를 한번 봐야할듯..
-   */
-  zz_addReservation(customer,isPriority) {
-    assert(isPriority == true || isPriority == false);
+  addReservation(customer, isPriority = false) {
     this.#reservations.push(customer);
   }
 
   hasReservation(customer) {
     return this.#reservations.some(
-      (reservedCustomer) => reservedCustomer.id === customer.id
+        (reservedCustomer) => reservedCustomer.id === customer.id
     );
   }
 }
