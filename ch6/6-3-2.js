e6b930acbf14d14dffa1d8823ca6bfdcffc841e5
zerom-h <cupdisin@gmail.com>
2023-01-02 11:06:15 +0900

    refactor: 변수 추출하기, aRecord -> record 변수명 변경

--- ch6/6-3-2.js
+++ ch6/6-3-2.js
@@ -1,28 +1,29 @@
 export class Order {
-  constructor(aRecord) {
-    this._data = aRecord;
+  #data;
+  constructor(record) {
+    this.#data = record;
   }
 
   get quantity() {
-    return this._data.quantity;
+    return this.#data.quantity;
   }
   get itemPrice() {
-    return this._data.itemPrice;
+    return this.#data.itemPrice;
   }
 
   get price() {
-    return  this.basePrice() - this.quantityDiscount() + this.shipping();
-  }
-
-  get shipping() {
-    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
-  }
-
-  get quantityDiscount() {
-    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+    return this.basePrice - this.discount + this.shipping;
   }
 
   get basePrice() {
     return this.quantity * this.itemPrice;
+  }
+
+  get discount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
   }
 }
