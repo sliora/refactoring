bbcb92cb0acf83c216988e3f36fb2651372a3bb4
zerom-H <cupdisin@gmail.com>
2023-01-07 18:42:26 +0900

    refactor: 함수 옮기기 - 다른 클래스로 옮기기

--- ch8/8-1-2.js
+++ ch8/8-1-2.js
@@ -4,18 +4,15 @@ export class Account {
     this._daysOverdrawn = daysOverdrawn;
   }
 
-  get bankCharge() {
+  get bankCharge() {  //은행 이자 계산
     let result = 4.5;
-    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    if (this._daysOverdrawn > 0)
+      result += this.type.overdraftCharge(this.daysOverdrawn);
     return result;
   }
 
   get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-    } else return this._daysOverdrawn * 1.75;
+    return this.type.overdraftCharge(this);
   }
 
   get daysOverdrawn() {
@@ -29,5 +26,18 @@ export class AccountType {
   }
   get isPremium() {
     return this._type === 'Premium';
+  }
+
+  //초과 인출 이자 계산, 소스 컨텍스트에서 가져와야 할 데이터가 많다면 account 를 아니라면 daysOverdrawn 을 넘긴다.
+  overdraftCharge(account) {
+    if (this.isPremium) {
+      const baseCharge = 10;
+      if (account.daysOverdrawn <= 7)
+        return baseCharge;
+      else
+        return baseCharge + (account.daysOverdrawn - 7) * 0.85;
+    }
+    else
+      return account.daysOverdrawn * 1.75;
   }
 }
