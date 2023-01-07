32568512d5e640b19eba909034ad7347e1daaa9f
zerom-H <cupdisin@gmail.com>
2023-01-07 19:11:29 +0900

    refactor: 필드 옮기기 - 공유 객체로 이동하기

--- ch8/8-2-2.js
+++ ch8/8-2-2.js
@@ -2,6 +2,7 @@ class Account {
   constructor(number, type, interestRate) {
     this._number = number;
     this._type = type;
+    assert(interestRate === this._type.interestRate);
     this._interestRate = interestRate;
   }
 
@@ -11,7 +12,12 @@ class Account {
 }
 
 class AccountType {
-  constructor(nameString) {
+  constructor(nameString, interestRate) {
     this._name = nameString;
+    this._interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this._type._interestRate;
   }
 }
