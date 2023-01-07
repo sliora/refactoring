5a22d7ab93c1a110144778f84eb70d4c533376fd
zerom-H <cupdisin@gmail.com>
2023-01-07 20:35:02 +0900

    refactor: 반복문을 파이프라인으로 바꾸기

--- ch8/8-8.js
+++ ch8/8-8.js
@@ -1,19 +1,11 @@
 export function acquireData(input) {
   const lines = input.split('\n');
-  let firstLine = true;
-  const result = [];
-  for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
-    if (line.trim() === '') continue;
-    const record = line.split(',');
-    if (record[1].trim() === 'India') {
-      result.push({ city: record[0].trim(), phone: record[2].trim() });
-    }
-  }
-  return result;
+  return lines
+      .slice  (1)
+      .filter (line   => line.trim() != '')
+      .map    (line   => line.split(','))
+      .filter (fields => fields[1].trim() === 'India')
+      .map    (fields => ({city: fields[0].trim(), phone: fields[2].trim()}));
 }
 
 const input = `office, country, telephone\n
