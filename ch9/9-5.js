97ed083005c5478e155f8c46d9a678792c932ff9
zerom-h <cupdisin@gmail.com>
2023-02-02 11:24:02 +0900

    refactor: 값을 참조로 바꾸기

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,12 +1,31 @@
+
+
 class Order {
   constructor(data) {
     this._number = data.number;
-    this._customer = new Customer(data.customerId);
+    this._customer = registerCustomer(data.customerId);
   }
 
   get customer() {
     return this._customer;
   }
+}
+
+let _repositoryData;
+
+export function initialize() {
+  _repositoryData = {};
+  _repositoryData.customers = new Map();
+}
+
+export function registerCustomer(customerId) {
+  if (! _repositoryData.customers.has(customerId))
+    _repositoryData.customers.set(customerId, new Customer(customerId));
+  return findCustomer(customerId);
+}
+
+export function findCustomer(customerId) {
+  return _repositoryData.customers.get(customerId);
 }
 
 class Customer {
@@ -18,3 +37,7 @@ class Customer {
     return this._id;
   }
 }
+
+initialize();
+const order = new Order({number: 123, customerId: 456});
+console.log(order.customer.id);
