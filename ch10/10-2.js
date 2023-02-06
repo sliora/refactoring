f1a052eb6b6a3abae378306551589b817bcf3e79
zerom-h <cupdisin@gmail.com>
2023-02-06 17:20:45 +0900

    refactor: 조건식 통합하기

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,6 +1,22 @@
-function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
-  return 1;
+function isNotEligibleForDisability(employee) {
+  return ((employee.seniority < 2) || (employee.monthsDisabled > 12) || (employee.isPartTime));
 }
+
+function disabilityAmount(employee) {
+  return isNotEligibleForDisability(employee) ? 1 : 0;
+}
+
+const employee1 = {
+  seniority: 3,
+  monthsDisabled: 10,
+  isPartTime: false
+};
+
+const employee2 = {
+  seniority: 1,
+  monthsDisabled: 20,
+  isPartTime: true
+};
+
+console.log(disabilityAmount(employee1)); // Output: 0
+console.log(disabilityAmount(employee2)); // Output: 1
