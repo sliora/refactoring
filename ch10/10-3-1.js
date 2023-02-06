4d320885ca11d5d88adbba5a46972f9f14bd2659
zerom-h <cupdisin@gmail.com>
2023-02-06 17:46:43 +0900

    refactor: 중첩 조건문을 보호 구문으로 바꾸기

--- ch10/10-3-1.js
+++ ch10/10-3-1.js
@@ -1,22 +1,40 @@
 export function payAmount(employee) {
-  let result;
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return { amount: 0, reasonCode: 'SEP' };
   }
-  return result;
+
+  if (employee.isRetired) {
+   return { amount: 0, reasonCode: 'RET' };
+  }
+
+  //급여 계산 로직
+  // lorem.ipsum(dolor.sitAmet);
+  // consectetur(adipiscing).elit();
+  // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  // ut.enim.ad(minim.veniam);
+  return someFinalComputation();
 }
 
+const employee1 = {
+  isSeparated: false,
+  isRetired: false,
+};
+
+const employee2 = {
+  isSeparated: true,
+  isRetired: false,
+};
+
+const employee3 = {
+  isSeparated: false,
+  isRetired: true,
+};
+
 function someFinalComputation() {
+
   return { amount: 999, reasonCode: 'UNICORN' };
 }
+
+console.log(payAmount(employee1)); // Output: someFinalComputation() 내용
+console.log(payAmount(employee2)); // Output: { amount: 0, reasonCode: 'SEP' }
+console.log(payAmount(employee3)); // Output: { amount: 0, reasonCode: 'RET' }
