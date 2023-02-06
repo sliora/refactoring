b9a3695da87ca0edfcb88f019be98de7b95cb168
zerom-h <cupdisin@gmail.com>
2023-02-06 17:46:40 +0900

    refactor: 중첩 조건문을 보호 구문으로 바꾸기

--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,18 @@
 export function adjustedCapital(instrument) {
   let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
-  }
+  if (instrument.capital <= 0|| instrument.interestRate <= 0 || instrument.duration < 0)  return result;
+
+    result = (instrument.income / instrument.duration) * instrument.adjustmentFactor;
+
   return result;
 }
+
+const instrument = {
+  capital: 1000,
+  interestRate: 0.05,
+  duration: 5,
+  income: 50,
+  adjustmentFactor: 1.5
+};
+
+console.log(adjustedCapital(instrument)); //Output: 15
