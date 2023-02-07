5235627d256caacc6bc2f9d6a1d3e0bb1a29ac4a
zerom-h <cupdisin@gmail.com>
2023-02-07 11:07:57 +0900

    refactor: 특이 케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -4,7 +4,7 @@ export class Site {
   }
 
   get customer() {
-    return this._customer;
+    return (this._customer === "미확인 고객" ) ? new UnknownCustomer() : this._customer;
   }
 }
 
@@ -30,13 +30,45 @@ export class Customer {
   }
 }
 
+class UnknownCustomer {
+
+  get name() {
+    return "거주자";
+  }
+
+  get billingPlan() {
+    return registry.billingPlan.basic;
+  }
+  set billingPlan(arg) {
+    /* 무시 */
+  }
+
+  get paymentHistory() {
+    return new NullPaymentHistory();
+  }
+
+  get weeksDelinquentInLastYear() {
+    return 0;
+  }
+}
+
 // 사용하는 부분
 export function customerName(site) {
   const aCustomer = site.customer;
-  // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
+  console.log(`aCustomer: ${aCustomer}`);
+  console.log(`aCustomer.name: ${aCustomer.name}`);
+
+  let customerName = aCustomer.name;
+  console.log(`customerName: ${customerName}`);
+
+  const plan = aCustomer.billingPlan;
+  console.log(`plan: ${plan}`);
+
+  aCustomer.billingPlan = newPlan;
+  console.log(`newPlan: ${newPlan}`);
+
+  const weeksDelinquent = aCustomer.paymentHistory.weeksDelinquentInLastYear;
+  console.log(`weeksDelinquent: ${weeksDelinquent}`);
 
   return customerName;
 }
