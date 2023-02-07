d6a81e6e872e4e63656abec730f1b8bb9afb65c1
zerom-h <cupdisin@gmail.com>
2023-02-07 11:00:57 +0900

    refactor: 어서션 추가하기

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,19 @@
+import * as assert from "assert";
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
-    return this.discountRate ? number - this.discountRate * number : number;
+    if(!this.discountRate) return number;
+    else {
+      assert(this.discountRate >= 0);
+      return number - (this.discountRate * number)
+    }
+  }
+
+  set discountRate(number) {
+    assert(null === number || number >= 0);
+    this.discountRate = number;
   }
 }
