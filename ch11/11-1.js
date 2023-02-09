2b1036f1c404ce172b79c37750fec2608cdf340c
zerom-H <cupdisin@gmail.com>
2023-02-09 23:22:45 +0900

    refactor: 질의 함수와 변경 함수 분리하기

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,34 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
+function totalOutstanding() {
   const result = customer.invoices.reduce(
     (total, each) => each.amount + total,
     0
   );
-  sendBill();
   return result;
 }
+function sendBill() {
+  sendBill();
+}
+
+
 
 // 예제 2
-export function alertForMiscreant(people, alarm) {
+
+function findMiscreant(people) {
   for (const p of people) {
     if (p === 'Don') {
-      setOffAlarms(alarm, p);
-      return 'Don';
+      return "Don";
     }
     if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
+      return "John"
     }
   }
-  return '';
+  return "";
 }
 
 function setOffAlarms(alarm, p) {
   alarm.setOff('Found Miscreant ' + p);
 }
+
+const found = findMiscreant(people);
+setOffAlarms(alarm, found);
