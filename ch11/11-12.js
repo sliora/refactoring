a3bd3f96178729ff0a6bb69ab1e70c57246aa7ff
zerom-h <cupdisin@gmail.com>
2023-02-13 12:30:42 +0900

    refactor: 오류 코드를 예외로 바꾸기

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -1,4 +1,38 @@
-function localShippingRules(data) {
+function localShippingRules(country) {
+  const data = country;
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw new OrderProcessingError(-23);
+}
+
+function calculateShippingCosts() {
+  //관련 없는 코드
+  const shippingRules = localShippingRules();
+  //더 관련 없는 코드
+}
+
+class OrderProcessingError extends Error {
+  #code
+  constructor(errorCode) {
+    super(`주문 처리 오류: ${errorCode}`);
+    this.#code = errorCode;
+  }
+
+  get code() {
+    return this.#code;
+  }
+  get name() {
+    return "OrderProcessingError";
+  }
+}
+
+
+try {
+  calculateShippingCosts();
+} catch (e) {
+  if(e instanceof OrderProcessingError) {
+    console.log({errorCode: e.code})
+  }
+  else{
+    throw e;
+  }
 }
