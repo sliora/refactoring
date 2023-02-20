c6dc26495e154cbfd311d15029fd3de291eda8a6
zerom-h <cupdisin@gmail.com>
2023-02-20 18:56:30 +0900

    refactor: 서브 클래스를 위임으로 바꾸기

--- ch12/12-10-1.js
+++ ch12/12-10-1.js
@@ -4,39 +4,77 @@ class Booking {
     this._date = date;
   }
 
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
+
   get hasTalkback() {
-    return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+    return (this._premiumDelegate)
+        ? this._premiumDelegate.hasTalkback
+        : this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+  }
+
+  get hasDinner() {
+    return (this._premiumDelegate)
+      ? this._premiumDelegate.hasDinner
+      : undefined;
   }
 
   get basePrice() {
     let result = this._show.price;
-
     if (this.isPeakDay) {
       result += Math.round(result * 0.15);
     }
+    return (this._premiumDelegate)
+      ? this._premiumDelegate.extendBasePrice(result)
+      : result;
 
+  }
+
+  static createBooking(show, date) {
+    return new Booking(show, date);
+  }
+
+  static createPremiumBooking(show, date, extras) {
+    const result =  new Booking(show, date, extras);
+    result._bePremium(extras);
     return result;
   }
 }
 
-class PremiumBooking extends Booking {
-  constructor(show, date, extras) {
-    super(show, date);
+class PremiumBookingDelegate {
+
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
     this._extras = extras;
   }
 
   get hasTalkback() {
-    return this._show.hasOwnProperty('talkback');
+    return this._host._show.hasOwnProperty('talkback');
   }
 
-  get basePrice() {
-    return Math.round(super.basePrice + this._extras.PremiumFee);
+  extendBasePrice(base) {
+    return Math.round(base + this._extras.premiumFee);
   }
 
   get hasDinner() {
     return this._extras.hasOwnProperty('dinner') && !this.isPeakDay;
   }
 }
+const show = {
+  price: 100,
+  talkback: true
+};
+const date = new Date();
+const extras = {
+  premiumFee: 50,
+  dinner: true
+};
 
-const booking = new Booking(show, date);
-const premiumBooking = new PremiumBooking(show, date, extras);
+
+//const booking = new Booking(show, date);
+const booking = Booking.createBooking(show,date);
+console.log(booking)
+//const premiumBooking = new PremiumBooking(show, date, extras);
+const premiumBooking = Booking.createPremiumBooking(show, date, extras);
+console.log(premiumBooking)
